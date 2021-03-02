de081fc7faf630c63ea8696c90f212356112c3c0
stutiid <stutichaturvedi12@gmail.com>
2021-03-03 00:12:29 +0530

    Added: for tie event flip more

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -15,7 +15,25 @@ done
 
 if [ $heads -eq $tails ]
 then
-	echo its a tie
+	heads=0
+	tails=0
+	while [[ $(($heads-$tails)) -lt 2 && $(($tails-$heads)) -lt 2 ]]
+	do
+		coin=$((RANDOM%2))
+   	if [ $coin -eq 0 ]
+   	then
+      	((heads++))
+   	else
+      	((tails++))
+   	fi
+	done
+	if [ $heads -gt $tails ]
+	then
+		echo heads winby $(($heads-$tails))
+	else
+		echo tails winby $(($tails-$heads))
+	fi
+#end of tie event
 elif [ $heads -gt $tails ]
 then
 	echo heads win by $(($heads-$tails))
